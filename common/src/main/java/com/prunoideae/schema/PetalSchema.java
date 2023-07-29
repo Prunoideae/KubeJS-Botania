bb1e1f83b3f1d7f503e3a8bf62712618a6a1083c
Li Junyu <jlijh@connect.ust.hk>
2023-07-29 15:30:10 +0800

    updated to 1.3.1

--- common/src/main/java/com/prunoideae/schema/PetalSchema.java
+++ common/src/main/java/com/prunoideae/schema/PetalSchema.java
@@ -1,13 +1,22 @@
 package com.prunoideae.schema;
 
+import dev.latvian.mods.kubejs.item.InputItem;
 import dev.latvian.mods.kubejs.recipe.RecipeKey;
 import dev.latvian.mods.kubejs.recipe.component.ItemComponents;
 import dev.latvian.mods.kubejs.recipe.schema.RecipeSchema;
 
 public interface PetalSchema {
-    RecipeKey<?> OUTPUT = ItemComponents.OUTPUT.key("output");
-    RecipeKey<?> INGREDIENTS = ItemComponents.INPUT_ARRAY.key("ingredients");
-    RecipeKey<?> REAGENT = ItemComponents.INPUT.key("reagent").defaultOptional();
+	RecipeKey<?> OUTPUT = ItemComponents.OUTPUT.key("output");
+	RecipeKey<?> INGREDIENTS = ItemComponents.INPUT_ARRAY.key("ingredients");
+	RecipeKey<?> REAGENT = ItemComponents.INPUT.key("reagent").optional(InputItem.of("#botania:seed_apothecary_reagent")).alwaysWrite();
 
-    RecipeSchema SCHEMA = new RecipeSchema(OUTPUT, INGREDIENTS, REAGENT);
+	RecipeSchema SCHEMA = new RecipeSchema(OUTPUT, INGREDIENTS, REAGENT);
+			/*
+			.constructor(OUTPUT, INGREDIENTS, REAGENT)
+			.constructor((recipe, schemaType, keys, from) -> {
+				recipe.setValue(OUTPUT, from.get(OUTPUT));
+				recipe.setValue(INGREDIENTS, from.get(INGREDIENTS));
+				recipe.setValue(REAGENT, InputItem.of("#botania:seed_apothecary_reagent"));
+			}, OUTPUT, INGREDIENTS);
+			 */
 }
